62d1c895d036e82e89512727c5286b806eb2b35b
Michael Wiedner <wiednermb@gmail.com>
2020-12-01 00:10:50 +0000

    Added a comment to main

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 // This is the Driver class. Repl is dumb and forces it to be called Main.java
+
+// Whenever you make a sizeable edit to the project, commit and push the change to GitHub via the version control tab on the left.
+// Also may not be a bad idea to make a manual copy as well. I don't have full confidence in Repl.
 public class Main {
-  
+
 }
