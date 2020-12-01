e420772d9e1936ebd3cdf904e21246ca057f395f
Michael Wiedner <wiednermb@gmail.com>
2020-12-01 00:13:10 +0000

    More comments

---
+++ Course.java
@@ -0,0 +1,4 @@
+// Whenever you make a sizeable edit to the project, commit and push the change to GitHub via the version control tab on the left.
+// Also may not be a bad idea to make a manual copy as well. I don't have full confidence in Repl.
+
+// Try your best to be rigorous with comments. It'll make it easier for others to follow your code and contribute
